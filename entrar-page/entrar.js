4ed3c2c45fe8451728c27304c5f6d2f66df429d5
Kauan Batista de Sousa <batistakauan598@gmail.com>
2025-08-24 14:04:51 -0300

    Atualizar o entrar.js

--- entrar-page/entrar.js
+++ entrar-page/entrar.js
@@ -10,7 +10,7 @@ function irParaLogin() {
   
 
     setTimeout(() => {
-        window.location.href = "../login-page/login.html";
+        window.location.href = "projeto-fbia/login-page/login.html";
     }, 1500);
 }
 
